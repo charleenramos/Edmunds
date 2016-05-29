bc98870f760d528fd3ea8f4c359520f73c5be7dc
charleenramos <oc.charleen@gmail.com>
2016-05-29 16:45:07 -0700

    Update splashPage1js.js

--- splashPage1js.js
+++ splashPage1js.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
    $(".delay-4_5s").css("color", "black").delay(12000).queue(function(next){
    			$("#wrapper").fadeOut().parent().css('background', 'white').queue(function(next2){
    				setTimeout(function(){
-   					window.location.replace("http://charleenramos.github.io/Edmunds");
+   					window.location.replace("http://charleenramos.github.io/Edmunds/VehicleSearchForm.html");
    				}, 500);
    				next2();
    			});
