f0ef56c698f237880d85fcf32d918eb044af3699
charleenramos <oc.charleen@gmail.com>
2016-05-29 16:43:26 -0700

    Add files via upload

---
+++ splashPage1js.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+
+		$('#picOne').fadeIn(1500).delay(3500).fadeOut(1500);
+	    $('#picTwo').delay(5000).fadeIn(1500);
+
+
+});
+
+$(document).ready(function(){
+
+   $(".delay-4_5s").css("color", "black").delay(12000).queue(function(next){
+   			$("#wrapper").fadeOut().parent().css('background', 'white').queue(function(next2){
+   				setTimeout(function(){
+   					window.location.replace("http://charleenramos.github.io/Edmunds");
+   				}, 500);
+   				next2();
+   			});
+   			next();
+   		}
+   	);
+});
+
+
