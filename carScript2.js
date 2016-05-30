ed6b27e548a69e0082f6e3eb33671bdab41db30b
charleenramos <oc.charleen@gmail.com>
2016-05-29 18:17:27 -0700

    Update carScript2.js

--- carScript2.js
+++ carScript2.js
@@ -4,5 +4,9 @@ SEARCH FORM
 
 $(document).on('pageinit', "#searchpage", function() {
 	/* 'ENTER' IN TEXTBOX EXECUTES SEARCH */
-		$('#search_btn').click();
-	});
+		$('#search_btn').click(){
+
+  alert( "Handler for .click() called." );
+
+});
+
